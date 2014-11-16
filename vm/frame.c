e3936ab31dbe91145988e70d0d6b5678f8901aab
Mitaka-Jin <adwight117@gmail.com>
2014-11-16 14:21:55 -0600

    Started swap, probably won't compile

--- vm/frame.c
+++ vm/frame.c
@@ -8,6 +8,8 @@
 #include "userprog/syscall.h"
 #include "lib/kernel/hash.h"
 #include "threads/loader.h"
+#include "lib/string.h"
+#include "threads/vaddr.h"
 
 int number=382;
 static struct frame *frames;
@@ -19,6 +21,7 @@ int free_frames;
 //so those outside frame.c can't lock and unlock
 void lock_frame (void);
 void unlock_frame (void);
+int evict(void);
 
 void
 lock_frame (){
@@ -59,10 +62,15 @@ frame_get (){
 	struct frame *frame;
 	/* no free mem, lets get some */
 	if(frame_number >= number) {
-		// lock_frame ();
-		// evict ();
-		// kpage = palloc_get_page ( PAL_USER | (zero ? PAL_ZERO : 0) );
-		// unlock_frame ();
+		lock_frame ();
+		int evindex = evict ();
+		frame = &frames[evindex]; 
+		// frame -> page = palloc_get_page ( PAL_USER /*| (zero ? PAL_ZERO : 0)*/ );
+		memset(frame->page,0,PGSIZE);
+		frame -> thread = t;
+		frame -> held = true;
+		frame -> pinned = false;
+		unlock_frame ();
 	}
 
 	/* There is room, lets fill it */
@@ -107,4 +115,35 @@ frame_corresponding_page(struct frame *frame){
 	uint8_t *ret_val = frame->page;
 	printf("Made it farther\n");
 	return ret_val;
+}
+
+/* Returns the index of the frame we plan on evicting in the
+	frame array. Uses the eviction algorithm we set up in this
+	function.
+*/
+int evict(){
+	uint32_t *pd = thread_current()->pagedir;
+	int i;
+	int evict = -1;
+	for(i = 0; i < number; i++){
+		if(!(&frames[i])->pinned){
+			if(!pagedir_is_accessed(pd, (&frames[i])->page) && !pagedir_is_dirty(pd, (&frames[i])->page)){
+				evict = i;
+			}
+		}
+	}
+	if(evict == -1){
+		for(i = 0; i < number; i++){
+			if(!(&frames[i])->pinned){
+				if(!pagedir_is_accessed(pd,(&frames[i])->page)){
+					evict = i;
+				}
+			}
+		}
+	}
+	for(i = 0; i < evict; i++){
+		pagedir_set_accessed(pd, (&frames[i])->page,false);
+	}
+	return evict;
+
 }
